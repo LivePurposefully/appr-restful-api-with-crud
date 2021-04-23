a6390a6e47f2f7a9e280fb27ee1a40d6c42898bf
livepurposefully <nafissah.92@gmail.com>
2021-04-22 20:08:42 -0400

    implemented PATCH and DELETE

--- src/main.go
+++ src/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var IdCounter int = 0
+var IdCounter string = "1"
 
 type Passive struct{
 	Name string `json:"passivename" binding:"required"`
@@ -13,16 +13,20 @@ type Passive struct{
 }
 
 type Champion struct{
-	Id int
+	Id string
 	Name string `json:"name" binding:"required"`
 	DamageSource string `json:"damagesource" binding:"required"`
 	PassiveAbility Passive `json:"passiveability" binding:"required"`
 }
 
-var Champions = make([]Champion, 0, 100)
+var LeagueChampions map[string]Champion = make(map[string]Champion, 0)
 
 func respondToGetChampion(c *gin.Context){
-	c.JSON(200, Champions)
+	allChampions := make([]Champion, 0)
+	for _, leagueChampion:= range LeagueChampions {
+		allChampions = append(allChampions, leagueChampion)
+	}
+	c.JSON(200, allChampions)
 }
 
 func respondToPostChampion(c *gin.Context){
@@ -30,29 +34,76 @@ func respondToPostChampion(c *gin.Context){
 	
 	err := c.ShouldBindJSON(&champion)
 	champion.Id = IdCounter
-	IdCounter += 1
 
-	log.Println(champion)
+	log.Println("Creating this chamption: ", champion)
 	log.Println(err)
 
 	if err != nil {
 		c.JSON(400, gin.H{"message": "Bad request, failed to create champion"})
 		return
 	}
-	Champions = append(Champions, champion)
-	c.JSON(200, gin.H{"message": "Success, created new champion"})
+	LeagueChampions[string(IdCounter)] = champion
+	c.JSON(201, gin.H{"message": "Success, created new champion"})
+	IdCounter += "1"
+
+	log.Println("This is the id counter after POST: ", IdCounter)
+}
+
+func checkChampionAvailability(c *gin.Context, id string) bool{
+	log.Println("This is the deleted ID:", id)
+
+	_, ok := LeagueChampions[id]
+	if ok == false {
+		c.JSON(404, gin.H{"message": "This champion wasn't found"})
+		return false
+	}
+
+	return true
 }
 
 
-var LeageChamps = make([]Champion, 0, 100)
+func respondToDeleteChampion(c *gin.Context){
+	id := c.Query("id")
+
+	if !checkChampionAvailability(c, id){
+		return
+	}
+
+	delete(LeagueChampions, id)
+
+	c.JSON(200, gin.H{"message": `The champion with has been deleted`})
+}
+
+func respondToPatchChampion(c *gin.Context){
+	var champion Champion
+	id := c.Query("id")
+
+
+	if !checkChampionAvailability(c, id){
+		return
+	}
+	
+	err := c.ShouldBindJSON(&champion)
+
+	log.Println("Creating this chamption: ", champion)
+	log.Println(err)
+
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Bad request, failed to create champion"})
+		return
+	}
+
+	LeagueChampions[id] = champion
+	c.JSON(200, gin.H{"message": "Modified champion"})
+}
 
 func main(){
 	r := gin.Default()
 
 	r.GET("/champion", respondToGetChampion)
 	r.POST("/champion", respondToPostChampion)
-	r.PATCH("/champion/:id", respondToGetChampion)
-	r.DELETE("/champion/:id", respondToGetChampion)
+	r.PATCH("/champion", respondToPatchChampion)
+	r.DELETE("/champion", respondToDeleteChampion)
 
 	r.Run()
 }
